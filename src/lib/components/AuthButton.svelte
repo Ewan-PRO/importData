dbb3ca7f2a284443850a01593f84d2d12eab0253
Your Name <ewan16270409@outlook.fr>
2025-04-10 18:09:25 +0200

    :bug: test button

--- src/lib/components/AuthButton.svelte
+++ src/lib/components/AuthButton.svelte
@@ -1,12 +1,3 @@
-<script lang="ts">
-	export let user: any;
-</script>
-
-<form method="POST" action="?/{user ? 'signOut' : 'signIn'}">
-	<button
-		type="submit"
-		class="me-2 mb-2 rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-	>
-		{user ? 'Se déconnecter' : 'Se connecter'}
-	</button>
+<form method="POST" action="?/test">
+	<button type="submit">Test Form</button>
 </form>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,19 +1,5 @@
-import type { Actions } from './$types';
-import { dev } from '$app/environment';
-
-export const actions: Actions = {
-	signIn: async ({ locals }) => {
-		const baseUrl = dev
-			? 'http://localhost:5173'
-			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io';
-
-		await locals.logtoClient.signIn(`${baseUrl}/callback`);
-	},
-	signOut: async ({ locals }) => {
-		const baseUrl = dev
-			? 'http://localhost:5173'
-			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io';
-
-		await locals.logtoClient.signOut(`${baseUrl}/`);
+export const actions = {
+	test: async () => {
+		return { success: true };
 	}
 };
