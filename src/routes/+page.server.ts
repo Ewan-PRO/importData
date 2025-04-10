eb77c08c6a44762c75bb0dc65ac784204d2b24bd
Your Name <ewan16270409@outlook.fr>
2025-04-10 19:31:14 +0200

    :bug: return to the past

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,19 @@
-export const actions = {
-	test: async () => {
-		return { success: true };
+import type { Actions } from './$types';
+import { dev } from '$app/environment';
+
+export const actions: Actions = {
+	signIn: async ({ locals }) => {
+		const baseUrl = dev
+			? 'http://localhost:5173'
+			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io';
+
+		await locals.logtoClient.signIn(`${baseUrl}/callback`);
+	},
+	signOut: async ({ locals }) => {
+		const baseUrl = dev
+			? 'http://localhost:5173'
+			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io';
+
+		await locals.logtoClient.signOut(`${baseUrl}/`);
 	}
 };
