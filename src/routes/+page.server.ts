a87f10fb791d481d3749ab96ed39ca3892e8f82e
Your Name <ewan16270409@outlook.fr>
2025-04-10 17:39:02 +0200

    Correction URL

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,14 +5,14 @@ export const actions: Actions = {
 	signIn: async ({ locals }) => {
 		const baseUrl = dev
 			? 'http://localhost:5173'
-			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io/callback';
+			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io/';
 
 		await locals.logtoClient.signIn(`${baseUrl}/callback`);
 	},
 	signOut: async ({ locals }) => {
 		const baseUrl = dev
 			? 'http://localhost:5173'
-			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io/callback';
+			: 'http://gc0wo8k0swkgcswkowkkw8o8.151.80.117.67.sslip.io/';
 
 		await locals.logtoClient.signOut(`${baseUrl}/`);
 	}
