b0c7905de020615b797c9037d8e7733946e540c1
Ewan Senergous <ewan16270409@outlook.fr>
2025-04-11 12:09:00 +0200

    Configuration pour déploiement en production

--- src/routes/callback/+page.server.ts
+++ src/routes/callback/+page.server.ts
@@ -1,18 +1,18 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, url }) => {
 	try {
-		if (!locals.user) {
-			return {};
-		}
+		// Cette ligne est essentielle pour traiter le callback OAuth
+		await locals.logtoClient.handleSignInCallback(url.toString());
 
+		// Une fois l'authentification traitée, redirigez vers la page d'accueil
 		throw redirect(302, '/');
 	} catch (error) {
 		if (error instanceof redirect) {
 			throw error;
 		}
 		console.error('Erreur lors de la gestion du callback:', error);
-		throw redirect(302, '/');
+		throw redirect(302, '/acces-refuse');
 	}
 };
