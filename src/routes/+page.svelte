d181239ba6037a4216a2d7366ee5ee8f70fe3a67
Ewan Senergous <ewan16270409@outlook.fr>
2025-04-11 09:46:32 +0200

    :bug: bug pull

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 					type="submit"
 					class="rme-2 mb-2 rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 				>
-					Se connecter1
+					Se connecter12
 				</button>
 			</form>
 		</div>
