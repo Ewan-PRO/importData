7f22c1ae9152436e51cd3152f7223d44df894c6e
Ewan-Senergous <ewan16270409@outlook.fr>
2025-08-21 16:36:39 +0200

    :bug: fix white color select:

--- src/lib/components/ui/select/select-trigger.svelte
+++ src/lib/components/ui/select/select-trigger.svelte
@@ -29,7 +29,7 @@
 	data-slot="select-trigger"
 	data-size={size}
 	class={cn(
-		"border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex w-full items-center justify-between gap-2 rounded-md border shadow-xs outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+		"border-input selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex w-full items-center justify-between gap-2 rounded-md border bg-white shadow-xs outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
 		sizeClasses[size],
 		className
 	)}
